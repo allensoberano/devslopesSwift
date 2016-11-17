fd607723c0f731dd37507e945c0a1033b19ae0ce
Allen Soberano <allensoberano@gmail.com>
2016-11-16 21:51:54 -0800

    scrollview finished

--- scrollviews/PageThatScroll/PageThatScroll/ViewController.swift
+++ scrollviews/PageThatScroll/PageThatScroll/ViewController.swift
@@ -17,7 +17,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        //originally had this code in "viewDidLoad" but the frame data isn't
+        //available yet until viewDidAppear
         var contentWidth: CGFloat = 0.0
+        
+        let scrollWidth = scrollView.frame.size.width
         
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
@@ -25,18 +33,20 @@ class ViewController: UIViewController {
             images.append(imageView)
             
             var newX: CGFloat = 0.0
-            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
             
             contentWidth += newX
             
             scrollView.addSubview(imageView)
-            imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
             
         }
         
+        scrollView.clipsToBounds = false
         scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
         
         print(images.count)
+
     }
 
     override func didReceiveMemoryWarning() {
